173b6a1ef72d859c11b694da4851d9e8cb81a065
stbam <stbamb@gmail.com>
2014-09-20 00:42:23 -0600

    More Facebook

--- js/facebook.js
+++ js/facebook.js
@@ -71,6 +71,7 @@ function statusChangeCallback(response) {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML = 'Thanks for logging in, ';// + response.name + '!';
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
     });
   }
