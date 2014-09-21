5c403834e2610338142f1c4fb1ea3f1203252460
stbam <stbamb@gmail.com>
2014-09-21 01:45:42 -0600

    More Facebook

--- js/facebook.js
+++ js/facebook.js
@@ -10,12 +10,12 @@ function statusChangeCallback(response) {
       testAPI();
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
+      document.getElementById('status').append = 'Please log ' +
         'into this app.';
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
+      document.getElementById('status').append = 'Please log ' +
         'into Facebook.';
     }
   }
