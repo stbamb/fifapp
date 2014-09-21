289b13e8d6c9606220be8ca6d7b5169c8afa0693
stbam <stbamb@gmail.com>
2014-09-21 02:37:38 -0600

    More Facebook

--- js/searchResults.js
+++ js/searchResults.js
@@ -1,10 +1,30 @@
 function searchByLeague()
 {
-	$('#page-content-wrapper2').html("<p></p>"); // para vaciar el contenido, alistar para la siguiente busqueda
+	$('#page-content-wrapper').html("<p></p>"); // para vaciar el contenido, alistar para la siguiente busqueda
 	Parse.initialize("Dv4oYn0Ri2yDSvvy5WjEDcflbUDCXwZlZvT5NReZ", "pAUAzo4IlTIXZfGSeGB4qOKgc0o2bK9ksxA84a02");
-	var clubleague = document.getElementById('fcleague-search').value;
+	var clubLeague = document.getElementById('fc-search').value;
 	imageURLs = [];
-	Parse.Cloud.run('searchByLeague', {league: clubleague}, {
+	Parse.Cloud.run('searchByLeague', {league: clubLeague}, {
+	  success: function(result) {
+	  	for (var i = 0; i < result.length; i++)
+	  	{
+		  	var object = result[i];
+		  	imageURLs.push(object.get('logo'));
+		  	$('#page-content-wrapper').append("<p>" + object.get('name') + "</p>");
+		  	$('#page-content-wrapper').append("<img src='" + imageURLs[i].url() + "'/>"); 
+		}
+	  },
+	  error: function(error) { }
+	});
+}
+
+function searchByName()
+{
+	$('#page-content-wrapper').html("<p></p>"); // para vaciar el contenido, alistar para la siguiente busqueda
+	Parse.initialize("Dv4oYn0Ri2yDSvvy5WjEDcflbUDCXwZlZvT5NReZ", "pAUAzo4IlTIXZfGSeGB4qOKgc0o2bK9ksxA84a02");
+	var clubName = document.getElementById('fc-search').value;
+	imageURLs = [];
+	Parse.Cloud.run('searchByName', {name: clubName}, {
 	  success: function(result) {
 	  	for (var i = 0; i < result.length; i++)
 	  	{
